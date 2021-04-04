fa2ff4cd50154225e69a1a860992ac7630fbe699
Vincenzo Romito <romito_vincenzo@libero.it>
2021-04-04 22:22:02 +0200

    added the nb convert method and now the functionsNumber function works with the parse tree method

--- NotebookAnalyzers/utils.py
+++ NotebookAnalyzers/utils.py
@@ -1,4 +1,7 @@
 import json
+import ast
+from notebooktoall.transform import transform_notebook
+
 def notebookToCode(filename):
     """The function takes the name of the desired .ipynb file in the target notebooks folder and return a python code string"""
     f=open("../TargetNotebooks/"+(filename),)
@@ -7,9 +10,20 @@ def notebookToCode(filename):
     for cell in data["cells"]:
         if cell["cell_type"] == 'code':
             code.append(''.join(cell["source"]))
-    code = ''.join(code)
+    code = '\n'.join(code)
     return code
 
-def functionsNumber(code):
-    """The function takes the a python code string and return the number of declared functions in it"""
-    return code.count("def ")
+def notebookToPyFile(filename):
+    #The function takes the name of the desired .ipynb file in the target notebooks folder, converts it into a .py file,
+    #and returns the name of the .py file
+    transform_notebook(ipynb_file="../TargetNotebooks/"+(filename), export_list=["py"])
+    return filename.replace(".ipynb",".py")
+
+def functionsNumber(filename):
+    #The function takes the desired .ipynb file in the target notebooks folder, 
+    #converts to a .py it thanks to the notebookToPyFile function
+    #and returns the number of function definitions thanks to the parse tree of the .py file
+    with open(filename, 'r') as f:
+        tree = ast.parse(f.read())
+        f_num=sum(isinstance(exp, ast.FunctionDef) for exp in tree.body)
+    return f_num
