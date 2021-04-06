ab35bba99107f780b2b7a5e29c4255877b9e7118
Vincenzo Romito <romito_vincenzo@libero.it>
2021-04-06 16:30:34 +0200

    fixed responsability

--- NotebookAnalyzers/utils.py
+++ NotebookAnalyzers/utils.py
@@ -8,23 +8,24 @@ def notebookToJson(filename):
     data = json.load(f)
     return data
 
-def notebookToCode(data):
-    """The function takes the JSON of the target notebook and returns a python code string"""
-    code=[]
-    for cell in data["cells"]:
-        if cell["cell_type"] == 'code':
-            code.append(''.join(cell["source"]))
-    code = '\n'.join(code)
-    return code
+#def notebookToCode(data):
+#    """The function takes the JSON of the target notebook and returns a python code string"""
+#    code=[]
+#    for cell in data["cells"]:
+#        if cell["cell_type"] == 'code':
+#            code.append(''.join(cell["source"]))
+#    code = '\n'.join(code)
+#    return code
 
-def notebookSyntaxTree(filename):
+def notebookToCode(filename):
     """The function takes the name of the desired .ipynb file in the target notebooks folder, and returns the python code sintax tree"""
     transform_notebook(ipynb_file="../TargetNotebooks/"+(filename), export_list=["py"])
     f = open(filename.replace(".ipynb",".py"),'r')
-    tree = ast.parse(f.read())
-    return tree
+    py_code = f.read()
+    return py_code
 
-def functionsNumber(tree):
+def functionsNumber(code):
     """The function takes a python code sintax tree and returns the number of function definitions"""
+    tree = ast.parse(code)
     f_num=sum(isinstance(exp, ast.FunctionDef) for exp in tree.body)
     return f_num
