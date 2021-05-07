d8a873c764eab53cbfe54227401e33dbbdc34127
Vincenzo Romito <romito_vincenzo@libero.it>
2021-05-07 15:17:50 +0200

    added endpoint for linters description

--- NotebookAnalyzers/main.py
+++ NotebookAnalyzers/main.py
@@ -3,13 +3,21 @@ import pynblint
 
 app = FastAPI()
 
+nb_linter_descr = {"id": "nb-linter", "description": "Lints Python Jupyter notebooks, one at a time."}
+repo_linter_descr = {"id": "repo-linter", "description": "Lints all Jupyter notebooks in a Python project."}
+
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'Vincenzo'}}
+    return {'data': {'name': 'This is a notebook linter API'}}
 
 
-@app.get('/notebook/{id}/empty_cells') #analyze_noteboo #analyze_repository
-def empty_cells(id: str):
-    nb_dict = pynblint.notebook_to_dict(id)
-    return {'data': pynblint.count_empty_cells(nb_dict)}
+@app.get('/linters')
+def linters_descr():
+    return {"data": [nb_linter_descr, repo_linter_descr]}
+
+
+#@app.get('/notebook/{id}/empty_cells') #analyze_noteboo #analyze_repository
+#def empty_cells(id: str):
+#    nb_dict = pynblint.notebook_to_dict(id)
+#    return {'data': pynblint.count_empty_cells(nb_dict)}
