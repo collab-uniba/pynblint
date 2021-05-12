3f5073c2ffc8b133711cc7e2c4049e9d710d1cb4
Vincenzo Romito <romito_vincenzo@libero.it>
2021-05-12 11:58:48 +0200

    fixed response body

--- NotebookAnalyzers/main.py
+++ NotebookAnalyzers/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 nb_linter = linters.NbLinter()
 repo_linter = linters.RepoLinter()
 
-linters_dict = {nb_linter.id: nb_linter.description, repo_linter.id: repo_linter.description}
+linters_dict = {
+    'nb-linter': nb_linter,
+    'repo-linter': repo_linter
+}
 
 
 @app.get('/')
@@ -17,13 +20,12 @@ def index():
 @app.get('/linters/{linter_id}')
 def get_linter(linter_id: str):
     if linter_id in linters_dict:
-        return {'data': {'id': linter_id, 'description': linters_dict[linter_id]}}
-        #return {
-        #    'data': {
-        #        "id": linters_dict[linter_id].id,
-        #        "description": linters_dict[linter_id].description
-        #        }
-        #    }
+        return {
+            'data': {
+                "id": linters_dict[linter_id].id,
+                "description": linters_dict[linter_id].description
+                }
+            }
     else:
         raise HTTPException(status_code=400, detail="Bad request")
 
