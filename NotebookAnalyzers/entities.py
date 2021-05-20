ff566aad6a89509cb627d4dc33dc9fae7532deee
Vincenzo Romito <romito_vincenzo@libero.it>
2021-05-20 13:38:03 +0200

    fixed various suggestions

--- NotebookAnalyzers/entities.py
+++ NotebookAnalyzers/entities.py
@@ -34,21 +34,19 @@ class Repository:
 
     notebooks = []  # List of Notebook objects
 
-    def retrieve_notebooks(self, repo_path):
-        for root, dirs, files in os.walk(repo_path):
+    def retrieve_notebooks(self):
+        for root, dirs, files in os.walk(self.path):
+            for dir in dirs:
+                os.chmod(path.join(root, dir), stat.S_IRWXU)
             for file in files:
+                os.chmod(path.join(root, file), stat.S_IRWXU)
                 if file.endswith(".ipynb"):
                     file_path = os.path.join(root, file).replace('\\', '/')
                     nb = Notebook(file_path)
                     nb.path = file_path[8:]
                     self.notebooks.append(nb)
                     os.remove(file[:-5] + "py")
-        for root, dirs, files in os.walk(repo_path):
-            for dir in dirs:
-                os.chmod(path.join(root, dir), stat.S_IRWXU)
-            for file in files:
-                os.chmod(path.join(root, file), stat.S_IRWXU)
-        shutil.rmtree(repo_path)
+        shutil.rmtree(self.path)
 
 
 class LocalRepository(Repository):
@@ -56,13 +54,17 @@ class LocalRepository(Repository):
     This class stores data about a local code repository
     """
 
-    def __init__(self, path: str):
-        project_path = path
-        if path.endswith('.zip'):
-            with zipfile.ZipFile(path, 'r') as zip_ref:
+    def __init__(self, local_path: str):
+        self.path = local_path
+        if local_path.endswith('.zip'):
+            with zipfile.ZipFile(local_path, 'r') as zip_ref:
                 zip_ref.extractall(config.data_path)
-                project_path = config.data_path+(path.split("/")[-1])[:-4]
-        self.retrieve_notebooks(project_path)
+                self.path = config.data_path + (local_path.split("/")[-1])[:-4]
+            self.retrieve_notebooks()
+        elif path.isdir(local_path):
+            self.retrieve_notebooks()
+        else:
+            raise Exception
 
 
 class GitHubRepository(Repository):
@@ -71,6 +73,6 @@ class GitHubRepository(Repository):
     """
 
     def __init__(self, github_url: str):
-        git.Git(config.data_path).clone(github_url)
-        repo_path = config.data_path+(github_url.split("/")[-1])
-        self.retrieve_notebooks(repo_path)
+        git.Git(config.data_path).clone(github_url,depth=1)
+        self.path = config.data_path + (github_url.split("/")[-1])
+        self.retrieve_notebooks()
