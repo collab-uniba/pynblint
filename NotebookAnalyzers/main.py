607e850e27874e17ed607916aebb3de74dc607f6
Vincenzo Romito <romito_vincenzo@libero.it>
2021-05-23 17:31:48 +0200

    main.py now follows the oo approach to notebook objects

--- NotebookAnalyzers/main.py
+++ NotebookAnalyzers/main.py
@@ -5,6 +5,8 @@ import pynblint
 import shutil
 import config
 import linters
+from entities import Notebook, GitHubRepository, LocalRepository
+from pathlib import Path
 
 app = FastAPI()
 nb_linter = linters.NbLinter()
@@ -23,39 +25,12 @@ def index():
 
 @app.post("/linters/nb-linter/")
 async def nb_lint(notebook: UploadFile = File(...), bottom_size: int = Form(4)):
-    with open(config.data_path + notebook.filename, "wb") as buffer:
+    with open( Path(config.data_path) / notebook.filename, "wb") as buffer:
         shutil.copyfileobj(notebook.file, buffer)
-    script = pynblint.notebook_to_script(notebook.filename)
-    nb_dict = pynblint.notebook_to_dict(notebook.filename)
-    response = {
-        "data": {
-            "notebookName": notebook.filename,
-            "notebookStats":
-                {
-                    "numberOfCells": pynblint.count_cells(nb_dict),
-                    "numberOfMDCells": pynblint.count_md_cells(nb_dict),
-                    "numberOfCodeCells": pynblint.count_code_cells(nb_dict),
-                    "numberOfRawCells": pynblint.count_raw_cells(nb_dict)
-                },
-            "lintingResults":
-                {
-                    "linearExecutionOrder": pynblint.has_linear_execution_order(nb_dict),
-                    "numberOfClassDefinitions": pynblint.count_class_defs(script),
-                    "numberOfFunctionDefinitions": pynblint.count_func_defs(script),
-                    "allImportsInFirstCell": pynblint.are_imports_in_first_cell(script),
-                    "numberOfMarkdownLines": pynblint.count_md_lines(nb_dict),
-                    "numberOfMarkdownTitles": pynblint.count_md_titles(nb_dict),
-                    "bottomMarkdownLinesRatio": pynblint.get_bottom_md_lines_ratio(nb_dict),
-                    "nonExecutedCells": pynblint.count_non_executed_cells(nb_dict),
-                    "emptyCells: ": pynblint.count_empty_cells(nb_dict),
-                    "bottomNonExecutedCells": pynblint.count_bottom_non_executed_cells(nb_dict, bottom_size),
-                    "bottomEmptyCells": pynblint.count_bottom_empty_cells(nb_dict, bottom_size)
-                }
-                }
-               }
-    os.remove(config.data_path + notebook.filename)
-    os.remove(notebook.filename[:-5] + "py")
-    return response
+    nb = Notebook.from_string(notebook.filename)
+    os.remove(Path(config.data_path) / notebook.filename)
+    #os.remove(notebook.filename[:-5] + "py") useless if we use notebook.from_string method
+    return nb.get_pynblint_results()
 
 
 @app.get('/linters/{linter_id}')
