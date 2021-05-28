bcaef7b7ea7f2f7bac95bb321b9d815656189888
Vincenzo Romito <romito_vincenzo@libero.it>
2021-05-28 16:41:18 +0200

    added fixture for IO operations

--- NotebookAnalyzers/test_sample.py
+++ NotebookAnalyzers/test_sample.py
@@ -4,63 +4,56 @@ import config
 from entities import Notebook
 from pathlib import Path
 
+@pytest.fixture
+def notebooks():
+    nb1 = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
+    nb2 = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
+    return {
+        "stroke-prediction-beginner-s-guide.ipynb":nb1,
+        "my-attempt-at-analytics-vidhya-job-a-thon.ipynb":nb2
+    }
 
-def test_count_cells():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["notebookStats"]["numberOfCells"] == 32
+def test_count_cells(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"]["numberOfCells"] == 32
 
-def test_count_md_cells():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["notebookStats"]["numberOfMDCells"] == 13
+def test_count_md_cells(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"]["numberOfMDCells"] == 13
 
-def test_count_code_cells():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["notebookStats"]["numberOfCodeCells"] == 19
+def test_count_code_cells(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"]["numberOfCodeCells"] == 19
 
-def test_count_raw_cells():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["notebookStats"]["numberOfRawCells"] == 0
+def test_count_raw_cells(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"]["numberOfRawCells"] == 0
 
-def test_has_linear_execution_order():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["linearExecutionOrder"] == True
+def test_has_linear_execution_order(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"]["linearExecutionOrder"] == True
 
-def test_count_class_defs():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["numberOfClassDefinitions"] == 0
+def test_count_class_defs(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"]["numberOfClassDefinitions"] == 0
 
-def test_count_func_defs():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["numberOfFunctionDefinitions"] == 1
+def test_count_func_defs(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"]["numberOfFunctionDefinitions"] == 1
 
-def test_are_imports_in_first_cell():
-    nb = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["allImportsInFirstCell"] == False
+def test_are_imports_in_first_cell(notebooks):
+    assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"]["allImportsInFirstCell"] == False
 
-def test_count_md_lines():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["numberOfMarkdownLines"] == 7
+def test_count_md_lines(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["numberOfMarkdownLines"] == 7
 
-def test_count_md_titles():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["numberOfMarkdownTitles"] == 2
+def test_count_md_titles(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["numberOfMarkdownTitles"] == 2
 
-def test_get_bottom_md_lines_ratio():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["bottomMarkdownLinesRatio"] == 0
+def test_get_bottom_md_lines_ratio(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["bottomMarkdownLinesRatio"] == 0
 
-def test_count_non_executed_cells():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["nonExecutedCells"] == 0
+def test_count_non_executed_cells(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["nonExecutedCells"] == 0
 
-def test_count_empty_cells():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["emptyCells"] == 3
+def test_count_empty_cells(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["emptyCells"] == 3
 
-def test_count_bottom_non_executed_cells():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["bottomNonExecutedCells"] == 0
+def test_count_bottom_non_executed_cells(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["bottomNonExecutedCells"] == 0
 
-def test_count_bottom_empty_cells():
-    nb = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-    assert nb.get_pynblint_results()["lintingResults"]["bottomEmptyCells"] == 2
+def test_count_bottom_empty_cells(notebooks):
+    assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"]["bottomEmptyCells"] == 2
