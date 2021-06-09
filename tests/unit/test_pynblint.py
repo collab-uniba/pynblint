a796268011ec01154c510671eb95c2f0dbf53965
Vincenzo Romito <romito_vincenzo@libero.it>
2021-06-09 16:44:44 +0200

    added other test scenarios

--- tests/unit/test_pynblint.py
+++ tests/unit/test_pynblint.py
@@ -1,93 +1,138 @@
 from pynblint.notebook import Notebook
 from pathlib import Path
+import pytest
 
 if __name__ == '__main__':
+    pytest.main()
 
-    @pytest.fixture(scope="module")
-    def notebooks():
-        nb1 = Notebook(Path(config.target_path) / "stroke-prediction-beginner-s-guide.ipynb")
-        nb2 = Notebook(Path(config.target_path) / "my-attempt-at-analytics-vidhya-job-a-thon.ipynb")
-        nb3 = Notebook(Path(config.target_path) / "Untitled.ipynb")
-        return {
-            "stroke-prediction-beginner-s-guide.ipynb": nb1,
-            "my-attempt-at-analytics-vidhya-job-a-thon.ipynb": nb2,
-            "Untitled.ipynb": nb3
-        }
+@pytest.fixture(scope="module")
+def notebooks():
+    nb1 = Notebook("../fixtures/EmptyNotebook.ipynb")
+    nb2 = Notebook("../fixtures/FullNotebook.ipynb")
+    nb3 = Notebook("../fixtures/JustMarkdown.ipynb")
+    nb4 = Notebook("../fixtures/DefectedNotebook.ipynb")
+    return {
+        "EmptyNotebook.ipynb": nb1,
+        "FullNotebook.ipynb": nb2,
+        "JustMarkdown.ipynb": nb3,
+        "DefectedNotebook.ipynb": nb4
+    }
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 3),
+    ("FullNotebook.ipynb", 15)
+])
+def test_count_cells(test_input, expected, notebooks):
+    assert notebooks[test_input].get_pynblint_results()["notebookStats"]["numberOfCells"] == expected
 
-    @pytest.mark.parametrize("test_input,expected", [
-        ("stroke-prediction-beginner-s-guide.ipynb", 32),
-        ("Untitled.ipynb", 2)
-    ])
-    def test_count_cells(test_input, expected, notebooks):
-        assert notebooks[test_input].get_pynblint_results()["notebookStats"]["numberOfCells"] == expected
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("FullNotebook.ipynb", 5)
+])
+def test_count_md_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["notebookStats"][
+                   "numberOfMDCells"] == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("JustMarkdown.ipynb", 0),
+    ("FullNotebook.ipynb", 9)
+])
+def test_count_code_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["notebookStats"][
+                   "numberOfCodeCells"] == expected
 
-    def test_count_md_cells(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"][
-                   "numberOfMDCells"] == 13
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("FullNotebook.ipynb", 1)
+])
+def test_count_raw_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["notebookStats"][
+                   "numberOfRawCells"] == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", False),
+    ("FullNotebook.ipynb", True)
+])
+def test_has_linear_execution_order(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "linearExecutionOrder"] == expected
 
-    def test_count_code_cells(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"][
-                   "numberOfCodeCells"] == 19
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 1),
+    ("FullNotebook.ipynb", 0)
+])
+def test_count_class_defs(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "numberOfClassDefinitions"] == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("FullNotebook.ipynb", 1)
+])
+def test_count_func_defs(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "numberOfFunctionDefinitions"] == expected
 
-    def test_count_raw_cells(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["notebookStats"][
-                   "numberOfRawCells"] == 0
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", False),
+    ("FullNotebook.ipynb", True)
+])
+def test_are_imports_in_first_cell(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "allImportsInFirstCell"] == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("FullNotebook.ipynb", 8)
+])
+def test_count_md_lines(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "numberOfMarkdownLines"] == expected
 
-    def test_has_linear_execution_order(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"][
-                   "linearExecutionOrder"] == True
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("FullNotebook.ipynb", 1)
+])
+def test_count_md_titles(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "numberOfMarkdownTitles"] == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", None),
+    ("FullNotebook.ipynb", 0.375)
+])
+def test_get_bottom_md_lines_ratio(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "bottomMarkdownLinesRatio"] == expected
 
-    def test_count_class_defs(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"][
-                   "numberOfClassDefinitions"] == 0
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("DefectedNotebook.ipynb", 2)
+])
+def test_count_non_executed_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "nonExecutedCells"] == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", 0),
+    ("DefectedNotebook.ipynb", 1)
+])
+def test_count_empty_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "emptyCells"] == expected
 
-    def test_count_func_defs(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"][
-                   "numberOfFunctionDefinitions"] == 1
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", None),
+    ("DefectedNotebook.ipynb", 1)
+])
+def test_count_bottom_non_executed_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "bottomNonExecutedCells"] == expected
 
-
-    def test_are_imports_in_first_cell(notebooks):
-        assert notebooks["stroke-prediction-beginner-s-guide.ipynb"].get_pynblint_results()["lintingResults"][
-                   "allImportsInFirstCell"] == False
-
-
-    def test_count_md_lines(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "numberOfMarkdownLines"] == 7
-
-
-    def test_count_md_titles(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "numberOfMarkdownTitles"] == 2
-
-
-    def test_get_bottom_md_lines_ratio(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "bottomMarkdownLinesRatio"] == 0
-
-
-    def test_count_non_executed_cells(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "nonExecutedCells"] == 0
-
-
-    def test_count_empty_cells(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "emptyCells"] == 3
-
-
-    def test_count_bottom_non_executed_cells(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "bottomNonExecutedCells"] == 0
-
-
-    def test_count_bottom_empty_cells(notebooks):
-        assert notebooks["my-attempt-at-analytics-vidhya-job-a-thon.ipynb"].get_pynblint_results()["lintingResults"][
-                   "bottomEmptyCells"] == 2
+@pytest.mark.parametrize("test_input,expected", [
+    ("EmptyNotebook.ipynb", None),
+    ("DefectedNotebook.ipynb", 1)
+])
+def test_count_bottom_empty_cells(test_input, expected,notebooks):
+    assert notebooks[test_input].get_pynblint_results()["lintingResults"][
+                   "bottomEmptyCells"] == expected
