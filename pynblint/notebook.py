5fedf12b19b234b1ffdcf1d714ffc1a43ae39877
Luigi Quaranta <luigi.quaranta@uniba.it>
2022-04-13 20:05:47 +0200

    Collect pylint results at the notebook level

--- pynblint/notebook.py
+++ pynblint/notebook.py
@@ -1,15 +1,28 @@
 import ast
+import tempfile
+from dataclasses import dataclass
+from io import StringIO
 from pathlib import Path
-from typing import List
+from typing import List, Optional, Tuple
 
 import nbconvert
 import nbformat
 from nbformat.notebooknode import NotebookNode
+from pylint import epylint
 from rich.abc import RichRenderable
 from rich.console import Console, ConsoleOptions, RenderResult
 
 from .cell import Cell, CellType
 from .config import settings
+
+
+@dataclass
+class PylintResult:
+    line: int
+    category: str
+    msg_id: str
+    symbol: str
+    msg: str
 
 
 class Notebook(RichRenderable):
@@ -37,6 +50,29 @@ class Notebook(RichRenderable):
         python_exporter = nbconvert.PythonExporter()
         python_exporter.exclude_markdown
         self.script, _ = python_exporter.from_notebook_node(self.nb_dict)
+
+        # Lint notebook code with Pylint
+        pylint_opts = '--msg-template="{line}:{category}:{msg_id}:{symbol}:{msg}"'
+        pylint_opts += " --disable=missing-module-docstring,missing-final-newline"
+
+        with tempfile.NamedTemporaryFile(mode="r+") as fp:
+            fp.write(self.script)
+            fp.seek(0)
+
+            pylint_result: Optional[Tuple[StringIO, StringIO]] = epylint.py_run(
+                fp.name + " " + pylint_opts, return_std=True
+            )
+
+            if pylint_result is not None:
+                pylint_stdout, pylint_stderr = pylint_result
+
+                if pylint_stderr.getvalue() == "":
+                    self.pylint_result_list: List[PylintResult] = []
+                    for row in pylint_stdout.getvalue().splitlines()[1:-5]:
+                        line, category, msg_id, symbol, msg = row.split(":")
+                        self.pylint_result_list.append(
+                            PylintResult(int(line), category, msg_id, symbol, msg)
+                        )
 
         # Extract the Python abstract syntax tree
         # (or set `has_invalid_python_syntax` to True)
