0069f99895e38f12ae8cba526e553c11de838dd4
Luigi Quaranta <luigi.quaranta@uniba.it>
2022-04-19 16:31:10 +0200

    Raise `max_multiline_python_comment`'s default to 4

--- pynblint/config.py
+++ pynblint/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
     final_cells: int = 3
     min_md_code_ratio: float = 0.3
     max_data_file_size: int = 10 * 1000000  # 10MB
-    max_multiline_python_comment: int = 3
+    max_multiline_python_comment: int = 4
 
     # TODO: custom validation: included_lints OR excluded lints must be None
     #       I.e., something like:
